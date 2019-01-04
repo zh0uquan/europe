ab261438661bf8fef3e34a1dc2e48c969a85e3ae
zh0uquan <zhouquan0x16@gmail.com>
2019-01-04 14:50:20 +0100

    add travis

--- europe/__main__.py
+++ europe/__main__.py
@@ -1,1 +1,8 @@
+from france import NAME as fname
+from germany import NAME as gname
+from italy import NAME as iname
+from spain import NAME as sname
+
 print("this is europe main package")
+
+print("this package contains [{} {} {} {}]".format(fname, gname, iname, sname))
