869fd30ecae479bdf95a44c10830a5d04cd8a231
zh0uquan <zhouquan0x16@gmail.com>
2019-01-11 16:14:08 +0100

    try to output color to terminal

--- tool.py
+++ tool.py
@@ -1,8 +1,11 @@
-import delegator
+# import delegator
+#
+# c = delegator.run("poetry run pytest -m france")
+#
+# print(c.out)
+#
+# if c.err:
+#     raise Exception("failed")
 
-c = delegator.run("poetry run pytest -m france")
-
-print(c.out)
-
-if c.err:
-    raise Exception("failed")
+with open("test-script.sh") as f:
+    f.write("poetry run pytest -m france")
