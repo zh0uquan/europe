2cb8336db09dfcf2dc814753989240adaffa6783
zh0uquan <zhouquan0x16@gmail.com>
2019-01-29 12:11:33 +0100

    add package & upload part for dev env

--- pac.py
+++ pac.py
@@ -1,11 +1,10 @@
 import os
 import re
-import json
-import toml
+
+import click
 import delegator
 import subprocess
 
-import click
 from jsonschema import validate
 from pathlib import Path
 from poetry.utils.toml_file import TomlFile
@@ -18,37 +17,31 @@ VERSION_RE = re.compile('version\s+=\s+"*(?P<version>(\d+\.\d+\.\d+))"*')
 
 ROOT_PATH = Path(".")
 REPO_NAME = re.compile(r"europe(\.\w+])*")
-AUTOGEN_REQ_INI = "autogen_requirements.ini"
-AUTOGEN_REQ_TXT = "autogen_requirements.txt"
+AUTOGEN_SETUP_PY = "setup.py"
+AUTOGEN_REQ_INI = "requirements.ini"
+AUTOGEN_REQ_TXT = "requirements.txt"
 AUTO_TEST = "autogen_test.sh"
-SETUP_PY = "setup.py"
 
 PAC_JSON_SCHEMA = {
     "definitions": {},
     "$schema": "http://json-schema.org/draft-07/schema#",
     "$id": "http://example.com/root.json",
     "type": "object",
-    "title": "The Root Schema",
     "required": ["package", "dependencies", "dev-dependencies"],
     "properties": {
         "package": {
             "$id": "#/properties/package",
             "type": "object",
-            "title": "The Package Schema",
             "required": ["name", "version"],
             "properties": {
                 "name": {
                     "$id": "#/properties/package/properties/name",
                     "type": "string",
-                    "title": "The Name Schema",
-                    "default": "",
                     "pattern": "^(.*)$",
                 },
                 "version": {
                     "$id": "#/properties/package/properties/version",
                     "type": "string",
-                    "title": "The Version Schema",
-                    "default": "",
                     "examples": ["0.1.0"],
                     "pattern": "^(.*)$",
                 },
@@ -57,14 +50,10 @@ PAC_JSON_SCHEMA = {
         "dependencies": {
             "$id": "#/properties/dependencies",
             "type": "object",
-            "title": "The Dependencies Schema",
-            "default": None,
-            "": "",
         },
         "dev-dependencies": {
             "$id": "#/properties/dev-dependencies",
             "type": "object",
-            "title": "The Dev-dependencies Schema",
         },
     },
 }
@@ -91,9 +80,25 @@ setup(
     description="AUTO GENERATED SCRIPT",
     packages=["{package_name}"],
     include_package_data=True,
-    zip_safe=True
+    zip_safe=True,
+    author="bot",
+    author_email="bot@fake.com"
 )
 """
+
+
+def cleanup(func):
+    def wraps(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            raise RuntimeError(e)
+        finally:
+            for file in [AUTOGEN_REQ_INI, AUTOGEN_REQ_TXT, AUTOGEN_SETUP_PY]:
+                if os.path.isfile(file):
+                    os.remove(file)
+
+    return wraps
 
 
 class ModifiedPackage(Package):
@@ -104,7 +109,6 @@ class ModifiedPackage(Package):
         self._old_version = old_version
         self._next_version = _get_next_version(version, old_version)
         super().__init__(name, self._next_version)
-
 
     @property
     def next_version(self):
@@ -130,7 +134,6 @@ def _get_next_version(version_constraint, old_version) -> str:
 
 
 def _get_old_version(pac_file) -> str:
-
     c = delegator.run(f"git show origin/master:{pac_file} | grep version")
     match = VERSION_RE.match(c.out)
     if not match:
@@ -225,40 +228,61 @@ def generate_requirements_text(requirements):
     os.remove(AUTOGEN_REQ_INI)
 
 
-def get_requirements(package, dev=False):
-    if dev:
+def get_requirements(package, test=False):
+    if test:
         requirements = package.requires + package.dev_requires
     else:
         requirements = package.requires
     return requirements
 
 
-def install_package(package):
-    with open("setup.py", "w+") as f:
+def generate_setup(package):
+    with open(AUTOGEN_SETUP_PY, "w+") as f:
         f.write(
             SETUP_PY_TEMPLATE.format(
                 package_name=package.name, package_version=package.version
             )
         )
 
-    try:
-        c1 = delegator.run("python setup.py install")
-        c2 = delegator.run(f"pip install -r {AUTOGEN_REQ_TXT}")
-        if c1.err or c2.err:
-            raise Exception(
-                f"""{c1.err}
-                
-                {c2.err}
-                """
-            )
-    except Exception as e:
-        raise RuntimeError(e)
+
+def upload_package(package, test=False):
+    c = delegator.run(f"python {AUTOGEN_SETUP_PY} sdist")
+    if c.err:
+        if "warning" in c.err or c.err == "":
+            print(c.out)
+        else:
+            raise RuntimeError(c.err)
+    print(c.out)
+
+    if test:
+        url = "http://localhost:8080/"
     else:
-        print(c1.out)
-        print(c2.out)
-    finally:
-        os.remove(AUTOGEN_REQ_TXT)
-        os.remove(SETUP_PY)
+        url = os.environ['PYPI_URL']
+    c = delegator.run(
+        f"twine upload -u {os.environ['TWINE_USERNAME']} -p "
+        f"{os.environ['TWINE_PASSWORD']} --repository-url {url} "
+        f"dist/{package.name}-{package.version}.tar.gz"
+    )
+    if c.err:
+        raise RuntimeError(c.err)
+    print(c.out)
+
+
+def install_package(package, test=False):
+    generate_setup(package)
+    c1 = delegator.run(f"python {AUTOGEN_SETUP_PY} install")
+    if test:
+        install_requires = f"pip install --no-cache-dir -r {AUTOGEN_REQ_TXT}"
+    else:
+        install_requires = f"pip install -r {AUTOGEN_REQ_TXT}"
+    c2 = delegator.run(install_requires)
+    for c in [c1, c2]:
+        if "warning" in c.err or c.err == "":
+            continue
+        raise RuntimeError(c.err, c.cmd, c.out)
+
+    print(c1.out)
+    print(c2.out)
 
 
 def run_package_tests(package):
@@ -294,6 +318,7 @@ def track_changed_paths():
 
 
 @click.command()
+@cleanup
 def scoped_test():
     c = delegator.run("git branch | grep \* | cut -d ' ' -f2")
     if c.err:
@@ -307,42 +332,69 @@ def scoped_test():
         Pac.create(path, modified=True).package for path in changed_paths
     ]
     pending_packages = [Pac.create(path).package for path in pending_paths]
-
     affected_packages = set()
 
-    changed_package_dict = {package.name: package for package in changed_packages}
+    package_names = {package.name: package for package in changed_packages}
 
     for package in pending_packages:
-        # skip the package we have changed
+        # make sure we skip the package we have changed
         if package in changed_packages:
             continue
 
         for dependency in package.requires:
             dependency_name = dependency.name
-            if dependency_name not in changed_package_dict:
+            if dependency_name not in package_names:
                 continue
 
             if package in affected_packages:
                 continue
 
             # pac's dependency is affected by changed packages
-            if dependency.accepts(changed_package_dict[dependency_name]):
+            if dependency.accepts(package_names[dependency_name]):
                 affected_packages.add(package)
 
     affected_packages = list(affected_packages)
 
     print(f"we have tracked changed packages: {changed_packages}")
-    print(f"we have found affected packages {affected_packages}")
+    print(f"we have found affected packages: {affected_packages}")
 
-    for package in changed_packages + affected_packages:
+    graph = dict()
+    for package in changed_packages:
+        # init anyway to make sure we have this index
+        graph[package.name] = []
+        for dep in package.requires:
+            if dep.name in package_names:
+                graph[package.name].append(dep.name)
+
+    known = []
+    while True:
+        graph_copy = graph.copy()
+        for pack_name in graph_copy.keys():
+            if len(graph[pack_name]) == 0:
+                known.append(pack_name)
+                del graph[pack_name]
+                continue
+
+            for dep in graph[pack_name]:
+                if dep in known:
+                    graph[pack_name].remove(dep)
+
+        if len(graph) == 0:
+            break
+
+    print(known)
+    test_order = [package_names[name] for name in known]
+
+    for package in test_order + affected_packages:
         # we need to assume all the requirements in package
         # is already built there, to let us to install from
         print(package.name)
         print("=" * 80)
         requirements = get_requirements(package, dev=True)
         generate_requirements_text(requirements)
-        install_package(package)
+        install_package(package, test=True)
         run_package_tests(package)
+        upload_package(package, test=True)
 
     # detect global conflicts by checking the requirements compiled together
     requirements = []
@@ -351,6 +403,7 @@ def scoped_test():
 
     # try to compile and see if there is a error
     generate_requirements_text(requirements)
+    print("test done")
 
 
 @click.command()
@@ -359,9 +412,35 @@ def merge():
 
     changed_pacs = [Pac.create(path, modified=True) for path in changed_paths]
     for pac in changed_pacs:
-        pac.local_config['package']['version'] = pac.package.next_version
+        pac.local_config["package"]["version"] = pac.package.next_version
         with open(pac.file.path, "w") as f:
             f.write(pac.local_config.as_string())
+
+
+@click.command()
+@click.option("-n", "--name", help="subpackage name")
+@cleanup
+def build(name):
+    """
+    Subpackage actions: build
+    """
+    if not name:
+        raise click.BadParameter("--name is required for subpackage name")
+    all_paths = set(Path(".").rglob("pac.toml"))
+    found = None
+    for path in all_paths:
+        local_config = TomlFile(path.as_posix()).read()
+        if local_config["package"]["name"] == name:
+            found = Pac.create(path)
+
+    if not found:
+        raise click.BadParameter(
+            "Can't find the toml file for subpackage name in the this repo"
+        )
+    package = found.package
+    generate_setup(package)
+    generate_requirements_text(get_requirements(package))
+    upload_package(package)
 
 
 @click.group()
@@ -370,16 +449,8 @@ def cli():
 
 
 cli.add_command(scoped_test, "test")
-# cli.add_command(publish, "release")
 cli.add_command(merge, "merge")
+cli.add_command(build, "build")
 
 if __name__ == "__main__":
     cli()
-
-    # from poetry.packages import Dependency
-    from poetry.installation import Installer
-
-    # d = Dependency("pytest", "^4.0.0")
-    # print(d.constraint)
-
-    # functionality pac install version
